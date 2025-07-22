342d818e8b44fce97986960b84e6bd3c8191a9be
imsuajeon <suajeon@naver.com>
2025-07-22 17:22:28 +0900

    수아의 브랜치

---
+++ src/pages/mypage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { User } from 'lucide-react';
+function mypage() {
+  return (
+    <div className="w-full flex justify-between">
+      <User />
+    </div>
+  );
+}
+
+export default mypage;
