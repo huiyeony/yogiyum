09572f5c14afad0b546357aaeebb7ee87d1bd23a
hazelnut4523 <70emwd05@mail.neria.link>
2025-07-23 10:25:02 +0900

    [feature] 리뷰, 식당, 유저 interface 추가

---
+++ src/entities/restaurant.ts
@@ -0,0 +1,53 @@
+export interface Restaurant {
+  id: number;
+
+  /// 식당 이름
+  name: string;
+
+  /// 식당 대표 사진 URL
+  thumbnailUrl: URL;
+
+  /// 위도
+  latitude: number;
+
+  /// 경도
+  longitude: number;
+
+  /// 연락처
+  telephone: string;
+
+  /// 영업 시간
+  openingHour: string;
+
+  /// 분야
+  category: RestaurantCategory;
+}
+
+export enum RestaurantCategory {
+  /// 한식
+  Korean = "Korean",
+
+  /// 양식
+  Western = "Western",
+
+  /// 아시아음식
+  Asia = "Asia",
+
+  /// 일식
+  Japanese = "Japanese",
+
+  /// 중식
+  Chinese = "Chinese",
+
+  /// 분식
+  Street = "Street",
+
+  /// 카페
+  Cafe = "Cafe",
+
+  /// 뷔페
+  Buffet = "Buffet",
+
+  /// 기타
+  Etc = "Etc",
+}
---
+++ src/entities/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  /// 사용자 ID (UUID)
+  id: string;
+
+  /// 사용자 이메일
+  email: string;
+
+  /// 가입일자
+  registerDate: Date;
+}
