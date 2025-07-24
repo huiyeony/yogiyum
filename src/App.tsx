b680f4ac2ef60cad0236b9c78f3880e52ac51b53
hazelnut4523 <70emwd05@mail.neria.link>
2025-07-24 15:45:20 +0900

    [feature] 홈, 식당 상세페이지 라우팅, 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login";
 import SignUpPage from "./pages/signup";
 import TempPage from "./pages/temp";
 import VerificationPage from "./pages/verification";
+import MainPage from "./pages/Main";
+import Layout from "./components/Layout";
+import RestaurantDetailPage from "./pages/RestaurantDetail";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/temp" element={<TempPage />} />
-      <Route path="/verification" element={<VerificationPage />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/restaurant/:id" element={<RestaurantDetailPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/temp" element={<TempPage />} />
+        <Route path="/verification" element={<VerificationPage />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/components/MainHeader.tsx
@@ -0,0 +1,82 @@
+import type { User } from "@/entities/user";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import supabase from "@/lib/supabase";
+
+export default function MainHeader() {
+  return (
+    <header className="flex flex-row w-full justify-between items-center py-4">
+      {/* 헤더 왼쪽 */}
+      <Link to="/">
+        <h1 className="text-xl font-bold">요기얌</h1>
+      </Link>
+
+      <div className="flex flex-row gap-2">
+        <UserButton />
+      </div>
+    </header>
+  );
+}
+
+function UserButton() {
+  const [user, setUser] = useState<User | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    supabase.auth
+      .getSession()
+      .then((res) => {
+        if (!res.data.session) {
+          return;
+        }
+
+        const uid = res.data.session.user.id;
+
+        return supabase
+          .from("users")
+          .select()
+          .eq("user_internal_id", uid)
+          .single();
+      })
+      .then(({ data }) => {
+        if (data) {
+          return;
+        }
+
+        setUser({
+          id: data.id,
+          nickname: data.nickname,
+          email: "asdf",
+          registerDate: data.created_at,
+        });
+      });
+  }, []);
+
+  if (user) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger>{user.nickname}</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>마이페이지</DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={async () => {
+              await supabase.auth.signOut();
+              setUser(null);
+              navigate("/");
+            }}
+          >
+            로그아웃
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  } else {
+    return <Link to="/login">로그인</Link>;
+  }
+}
