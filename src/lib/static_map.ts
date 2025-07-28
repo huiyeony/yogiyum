fd3c0aa3567dc7d290a383a060c7af26ff64006a
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-07-28 16:38:10 +0900

    구글 클라우드 키 유출된 것 삭제

--- src/lib/static_map.ts
+++ src/lib/static_map.ts
@@ -2,7 +2,7 @@ export default function staticMapUrl(latitude: string, longitude: string): URL {
   const baseUrl = new URL("https://maps.googleapis.com/maps/api/staticmap");
 
   const searchParam = {
-    key: "AIzaSyAN4SbXntRdfTWEP_e66bZNj8GHaRjvtAM",
+    key: import.meta.env.VITE_GCP_KEY,
     markers: `size:mid,color:red|${latitude},${longitude}`,
     size: "540x304",
     scale: "2",
